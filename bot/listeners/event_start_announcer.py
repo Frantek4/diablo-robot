da8af76dde1ea5231d2f2d0e3c25b7ac5315f2bf
FranciscoB <barrosvito.francisco1@gmail.com>
2025-11-29 20:22:01 -0300

    Setup faltante

--- bot/listeners/event_start_announcer.py
+++ bot/listeners/event_start_announcer.py
@@ -7,3 +7,6 @@ class EventStartAnnouncer(commands.Cog):
     @commands.Cog.listener()
     async def on_scheduled_event_start(self, event):
         self.bot.messager.announce(f"{event.name} está por empezar")
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(EventStartAnnouncer(bot))
